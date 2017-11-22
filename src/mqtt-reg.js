e29a99ad9b29cb625a850b9760ce7b4b2aae9e4a
Pavel Burgr <pb@drake.cz>
2017-11-22 17:06:23 +0100

    WIP

--- src/mqtt-reg.js
+++ src/mqtt-reg.js
@@ -4,76 +4,75 @@ module.exports = (broker, device, register, cb, timeoutMs = 5000) => {
 
 	const client = mqtt.connect(`mqtt://${broker}`);
 
-	let readTimeout;
-
+	let timeout;
+	let firstTimeout;
 	let actual;
 	let desired;
 
+
 	function safeCb(actual, prev) {
 		try {
-			cb(actual, prev);			
+			cb(actual, prev);
 		} catch (e) {
 			console.error("Error in register callback", e);
 		}
 	}
 
-	function is(v) {
-		let prev = actual;
-		actual = v;
-
-		if (actual !== prev) {
-			safeCb(actual, prev);
-		}
-
-		if (actual !== undefined && desired === actual) {
-			desired = undefined;
-		}
-
-		if (desired !== undefined) {
-			set();
-		} else {
-			if (actual === undefined) {
-				get();
-			}
-		}
-
-	}
-
 	function resetTimeout() {
-		if (readTimeout) {
-			clearTimeout(readTimeout);
+		
+		if (timeout) {
+			clearTimeout(timeout);
 		}
-		readTimeout = setTimeout(() => {
-			is();
+		
+		timeout = setTimeout(() => {
+			
+			if (!firstTimeout) {
+				let prev = actual;
+				actual = undefined;
+				if (actual !== prev) {
+					safeCb(actual, prev);
+				}				
+			}
+			
+			firstTimeout = false;
+			getOrSet();
+			resetTimeout();
+			
 		}, timeoutMs);
 	}
 
-	function set() {
-		resetTimeout();
-		client.publish(`${device}/register/${register}/set`, JSON.stringify(desired));
-	}
-
-	function get() {
-		resetTimeout();
-		client.publish(`${device}/register/${register}/get`);
+	function getOrSet() {
+		if (desired !== undefined) {
+			client.publish(`${device}/register/${register}/set`, JSON.stringify(desired));
+		} else {
+			client.publish(`${device}/register/${register}/get`);
+		}
 	}
 
 	client.subscribe(`${device}/register/${register}/is`);
 
 	client.on("message", function (topic, message) {
+
+		firstTimeout = true;
+		let prev = actual;
+		actual = JSON.parse(message.toString());
+
+		if (actual !== prev) {
+			safeCb(actual, prev);
+		}
+
 		resetTimeout();
-		let value = JSON.parse(message.toString());
-		is(value);
 	});
 
-	get();
+	getOrSet();
 	safeCb();
+	resetTimeout();
 
 	return {
 		set(value) {
 			if (desired !== value && actual !== value) {
 				desired = value;
-				set();
+				getOrSet();
 			}
 		},
 		actual() {
